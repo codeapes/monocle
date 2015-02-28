db92890d854dfa5ed0dea915912a4afa7464c0e1
dlegien <daniel@legien.org>
2015-02-28 09:57:32 +0100

    Added untracked files, doh.

---
+++ Monocle/Testing/TestCategory.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2015 All Right Reserved, http://CodeApes.de/
+
+namespace CodeApes.Monocle
+{
+    /// <summary>
+    /// Categories for unit tests.
+    /// </summary>
+    public static class TestCategory
+    {
+        /// <summary>
+        /// Category for tests that require no additional resources.
+        /// </summary>
+        public const string ContinousIntegration = "CI";
+    }
+}

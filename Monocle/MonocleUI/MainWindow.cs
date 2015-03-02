94faac995ad32f31e759bf2f08d66939936cc798
Stefan Legien <stefan@legien.org>
2015-03-02 20:12:03 +0100

    Adding UI project to monocle solution.
    Includes SolutionOpenCommand, part of #1.

---
+++ Monocle/MonocleUI/MainWindow.cs
@@ -0,0 +1,51 @@
+﻿namespace MonocleUI
+{
+	using Eto.Forms;
+	using Eto.Drawing;
+
+	public class MainWindow : Form
+	{
+		private string title;
+
+		public MainWindow ()
+		{
+			Menu = new MenuBar ();
+			SetTitle (null);
+			ShowInTaskbar = true;
+			Maximize ();
+			AddFileMenuItem (new FileMenuOpenCommand ());
+			AddFileMenuItem (new SolutionOpenCommand ());
+			AddApplicationMenuItem (new SeparatorMenuItem());
+		}
+
+		public void AddApplicationMenuItem(MenuItem menuItem) 
+		{
+			Menu.ApplicationMenu.Items.Add (menuItem);
+		}
+
+		public void AddFileMenuItem(MenuItem menuItem)
+		{
+			((MenuItemCollection)Menu.Items).GetSubmenu("File",0,true,true).Items.Add(menuItem);
+		}
+
+		public void SetTitle(string title)
+		{
+			if (title != null) 
+			{
+				this.title = title;
+				this.Title = "Monocle - " + this.title;
+			} 
+			else 
+			{
+				this.title = "Monocle";
+				this.Title = this.title;
+			}
+		}
+
+		public string GetCurrentTitle() 
+		{
+			return this.title;
+		}
+	}
+}
+
---
+++ Monocle/MonocleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MonocleUI
+{
+	using System;
+	using Eto.Forms;
+
+	public class Program
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			var application = new Application (Eto.Platforms.Mac);
+			var mainWindow = new MainWindow ();
+			application.Run (mainWindow);
+		}
+	}
+}
